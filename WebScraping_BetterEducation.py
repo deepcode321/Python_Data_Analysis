642172d0678e17e6604fee48bd12d545c610f1f3
Deepa prabakaran <93232604+deepcode321@users.noreply.github.com>
2023-03-30 11:54:03 +1100

    Add files via upload

---
+++ WebScraping_BetterEducation.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import pandas as pd
+
+html_content = """
+<html>
+<body>
+<p>This HTML content doesn't contain any tables.</p>
+</body>
+</html>
+"""
+
+tables = pd.read_html(html_content)
+
+if len(tables) == 0:
+    print("No tables were found.")
+else:
+    print("Tables were found.")
+
+
+# In[97]:
+
+
+import requests
+
+#download the HTML document
+#with an HTTP GET request
+response = requests.get("https://bettereducation.com.au/Results/vce.aspx")
+
+#print the HTML code
+#print(response.text)
+soup = BeautifulSoup(response.text, 'html.parser')
+table = soup.find('table', class_ = 'table table-striped table-bordered table-hover')
+school_list = []
+show_list = []
+
+for school_data in table.find_all('tbody'):
+    rows = school_data.find_all('tr')
+    
+print("Rank\t\t","School Name\t\t", "Percentile\t\t", "Median\t\t", "Location\t\t")   
+
+for row in rows:
+    rank = row.find_all('td')[1].text.replace("\n", "").replace("\r","")
+    rank = rank.split(',')
+    school_list.append(rank)
+    
+    school = row.find_all('td')[2].text.replace("\n", "").replace("\r","")
+    school = school.split(',')[0]
+    school_list.append(school)
+   
+        
+    scores = row.find_all('td')[3].text.replace("\n", "").replace("\r","")
+    scores = scores.strip()
+    school_list.append(scores)
+    
+    median = row.find_all('td')[4].text.replace("\n", "").replace("\r","")
+    school_list.append(median)
+    
+    locality = row.find_all('td')[6].text.replace("\n", "").replace("\r","")
+    school_list.append(locality)
+    
+    show_list = school_list.copy()
+    school_list.clear()
+    print(show_list)
+    
+    
+    
+    
+    
+ 
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
